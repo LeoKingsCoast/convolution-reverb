01619b6244193fa5e3fe2bc9a4774c1bf94fca54
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-10-28 18:34:54 -0300

    Implement circular convolution

--- src/convolution.c
+++ src/convolution.c
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-Convolutor* conv_init(int input_size, int impulse_resp_size, float* impulse_resp) {
+Convolutor* conv_init_linear(int input_size, int impulse_resp_size, float* impulse_resp) {
     if (input_size <= 0 || impulse_resp_size <= 0 || impulse_resp == NULL)
         return NULL;
 
@@ -34,12 +34,9 @@ Convolutor* conv_init(int input_size, int impulse_resp_size, float* impulse_resp
     c->impulse_resp = malloc(c->impulse_resp_size * sizeof(float));
     c->window = malloc(c->window_size * sizeof(float));
     c->overlap = malloc(c->overlap_size * sizeof(float));
-    c->out_block = malloc(c->window_size * sizeof(float));
     c->output = malloc(c->output_size * sizeof(float));
 
-    c->window_freq = fftwf_malloc((c->window_size / 2 + 1) * sizeof(fftwf_complex));
     c->impulse_freq_padded = fftwf_malloc((c->window_size / 2 + 1) * sizeof(fftwf_complex));
-    c->out_block_freq = fftwf_malloc((c->window_size / 2 + 1) * sizeof(fftwf_complex));
 
     c->in_time_buffer = malloc(c->window_size * sizeof(float));
     c->in_freq_buffer = fftwf_malloc((c->window_size / 2 + 1) * sizeof(fftwf_complex));
@@ -48,10 +45,9 @@ Convolutor* conv_init(int input_size, int impulse_resp_size, float* impulse_resp
     c->fft_plan = fftwf_plan_dft_r2c_1d(c->window_size, c->in_time_buffer, c->out_freq_buffer, FFTW_MEASURE);
     c->ifft_plan = fftwf_plan_dft_c2r_1d(c->window_size, c->in_freq_buffer, c->out_time_buffer, FFTW_MEASURE);
 
-    if (!c->input || !c->window || !c->overlap || !c->out_block || !c->output ||
-        !c->window_freq || !c->impulse_freq_padded || !c->out_block_freq ||
-        !c->in_time_buffer || !c->in_freq_buffer || !c->out_time_buffer ||
-        !c->out_freq_buffer || !c->fft_plan || !c->ifft_plan) 
+    if (!c->input || !c->window || !c->overlap || !c->output ||
+        !c->impulse_freq_padded || !c->in_time_buffer || !c->in_freq_buffer ||
+        !c->out_time_buffer || !c->out_freq_buffer || !c->fft_plan || !c->ifft_plan)
     {
         free(c);
         return NULL;
@@ -73,7 +69,50 @@ Convolutor* conv_init(int input_size, int impulse_resp_size, float* impulse_resp
     return c;
 }
 
-void conv_terminate(Convolutor* c) {
+ConvolutorCircular* conv_init_circular(int input_size, int impulse_resp_size, float* impulse_resp) {
+    if (input_size <= 0 || impulse_resp_size <= 0 || impulse_resp == NULL)
+        return NULL;
+
+    ConvolutorCircular* c = (ConvolutorCircular*) malloc(sizeof(ConvolutorCircular));
+    if (!c)
+        return NULL;
+
+    c->input_size = input_size;
+
+    c->input = malloc(c->input_size * sizeof(float));
+    c->output = malloc(c->input_size * sizeof(float));
+
+    c->impulse_freq_padded = fftwf_malloc((c->input_size / 2 + 1) * sizeof(fftwf_complex));
+
+    c->in_time_buffer = malloc(c->input_size * sizeof(float));
+    c->in_freq_buffer = fftwf_malloc((c->input_size / 2 + 1) * sizeof(fftwf_complex));
+    c->out_time_buffer = malloc(c->input_size * sizeof(float));
+    c->out_freq_buffer = fftwf_malloc((c->input_size / 2 + 1) * sizeof(fftwf_complex));
+    c->fft_plan = fftwf_plan_dft_r2c_1d(c->input_size, c->in_time_buffer, c->out_freq_buffer, FFTW_MEASURE);
+    c->ifft_plan = fftwf_plan_dft_c2r_1d(c->input_size, c->in_freq_buffer, c->out_time_buffer, FFTW_MEASURE);
+
+    if (!c->input || !c->output || !c->impulse_freq_padded ||
+        !c->in_time_buffer || !c->in_freq_buffer || !c->out_time_buffer ||
+        !c->out_freq_buffer || !c->fft_plan || !c->ifft_plan)
+    {
+        free(c);
+        return NULL;
+    }
+
+    // Copy the impulse response to the fft input, padding with 0 until the window size
+    for (int i = 0; i < c->input_size; i++) {
+        if (i < impulse_resp_size) c->in_time_buffer[i] = impulse_resp[i];
+        else c->in_time_buffer[i] = 0;
+    }
+
+    // impulse response DFT stored in out_freq_buffer
+    fftwf_execute(c->fft_plan);
+    memcpy(c->impulse_freq_padded, c->out_freq_buffer, (c->input_size / 2 + 1) * sizeof(fftwf_complex));
+
+    return c;
+}
+
+void conv_terminate_linear(Convolutor* c) {
     if (c) {
         fftwf_destroy_plan(c->ifft_plan);
         fftwf_destroy_plan(c->fft_plan);
@@ -81,21 +120,34 @@ void conv_terminate(Convolutor* c) {
         free(c->out_time_buffer);
         fftwf_free(c->in_freq_buffer);
         free(c->in_time_buffer);
-        fftwf_free(c->window_freq);
         fftwf_free(c->impulse_freq_padded);
-        fftwf_free(c->out_block_freq);
         free(c->input);
         free(c->impulse_resp);
         free(c->window);
         free(c->overlap);
-        free(c->out_block);
         free(c->output);
 
         free(c);
     }
 }
 
-void conv_convolve(Convolutor* c) {
+void conv_terminate_circular(ConvolutorCircular* c) {
+    if (c) {
+        fftwf_destroy_plan(c->ifft_plan);
+        fftwf_destroy_plan(c->fft_plan);
+        fftwf_free(c->out_freq_buffer);
+        free(c->out_time_buffer);
+        fftwf_free(c->in_freq_buffer);
+        free(c->in_time_buffer);
+        fftwf_free(c->impulse_freq_padded);
+        free(c->input);
+        free(c->output);
+
+        free(c);
+    }
+}
+
+void conv_linear(Convolutor* c) {
     // After surpassing the input size, in_pos continues so that it stores the last block in the output
     for (int in_pos = 0, window_pos = 0; in_pos < c->input_size + c->window_size; in_pos++, window_pos++) {
         // If the FFT input is filled
@@ -152,5 +204,37 @@ void conv_convolve(Convolutor* c) {
             else
                 c->output[in_pos - c->impulse_resp_size] = c->out_time_buffer[window_pos] / c->window_size;
         }
+    }
+}
+
+void conv_circular(ConvolutorCircular* c) {
+    for (int i = 0; i < c->input_size; i++) {
+        c->in_time_buffer[i] = c->input[i];
+    }
+
+    // Store DFT of this window in out_freq_buffer
+    fftwf_execute(c->fft_plan);
+
+    /*
+     * Multiply window frequency signal by the impulse response spectrum
+     * Complex Multiplication: (a + jb)(c + jd) = (ac - bd) + j(ad + bc)
+     */
+    for (int i = 0; i < (c->input_size / 2 + 1); i++) {
+        c->in_freq_buffer[i][0] = c->out_freq_buffer[i][0] * c->impulse_freq_padded[i][0]
+            - c->out_freq_buffer[i][1] * c->impulse_freq_padded[i][1];
+        c->in_freq_buffer[i][1] = c->out_freq_buffer[i][0] * c->impulse_freq_padded[i][1]
+            + c->out_freq_buffer[i][1] * c->impulse_freq_padded[i][0];
+    }
+
+    /*
+     * Go back to time domain
+     * Store this window's convolution result in out_time_buffer
+     * We need to normalize the IFFT result every time we use it (divide
+     * by window_size)
+     */
+    fftwf_execute(c->ifft_plan);
+
+    for (int i = 0; i < c->input_size; i++) {
+        c->output[i] = c->out_time_buffer[i] / c->input_size;
     }
 }
--- src/convolution.h
+++ src/convolution.h
@@ -13,11 +13,7 @@ typedef struct convolutor {
 
     // --- Auxiliary variables ----
     float* window;
-    fftwf_complex* window_freq;
     int window_size;
-
-    float* out_block;
-    float* out_block_freq;
 
     float* overlap;
     int overlap_size;
@@ -31,6 +27,24 @@ typedef struct convolutor {
     fftwf_plan ifft_plan;
 } Convolutor;
 
+typedef struct convolutor_circular {
+    fftwf_complex* impulse_freq_padded; // Impuse response frequency signal, of the time signal padded to the window size
+
+    float* input; // Input time signal
+    int input_size; // Input frequency signal
+
+    float* output; // Output time signal
+
+    // --- Auxiliary variables ----
+    float* in_time_buffer;
+    fftwf_complex* in_freq_buffer;
+    float* out_time_buffer;
+    fftwf_complex* out_freq_buffer;
+
+    fftwf_plan fft_plan;
+    fftwf_plan ifft_plan;
+} ConvolutorCircular;
+
 /**
  * @brief Initializes a Convolutor for a given real input time signal and a 
  * real impulse response time signal. The input should be filled before doing 
@@ -38,14 +52,28 @@ typedef struct convolutor {
  */
 Convolutor* conv_init(int input_size, int impulse_resp_size, float* impulse_resp);
 
+ConvolutorCircular* conv_init_circular(int input_size, int impulse_resp_size, float* impulse_resp);
+
 /**
- * @brief Calculates the convolution between the input and imppulse_resp buffers
+ * @brief Calculates the convolution of the input and imppulse_resp buffers
  * in the Convolutor object. The result is stored in the output buffer. The
  * input and impulse_resp buffers should be filled before calling this function.
  */
-void conv_convolve(Convolutor*);
+void conv_linear(Convolutor*);
 
 /**
- * @brief Terminates the convolutor object. Returns 0 on success and -1 on failure.
+ * @brief Calculates the circular convolution of the input and imppulse_resp 
+ * buffers in the Convolutor object. The result is stored in the output buffer. The
+ * input and impulse_resp buffers should be filled before calling this function.
  */
-void conv_terminate(Convolutor*);
+void conv_circular(ConvolutorCircular* c);
+
+/**
+ * @brief Terminates the convolutor object.
+ */
+void conv_terminate_linear(Convolutor*);
+
+/**
+ * @brief Terminates the circular convolutor object.
+ */
+void conv_terminate_circular(ConvolutorCircular*);
